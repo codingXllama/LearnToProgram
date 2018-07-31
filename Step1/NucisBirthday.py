1426c3600afd019918914cfbf5edb82a6776fe00
Osama A <41015632+codingXllama@users.noreply.github.com>
2018-07-31 00:29:07 -0400

    Update NucisBirthday.py

--- Step1/NucisBirthday.py
+++ Step1/NucisBirthday.py
@@ -1,5 +1,7 @@
 age = int(input("Enter your Birthday: "))
-personName = input("Enter your name:")
+personName = input("Enter your name:").lower()
 
-if personName != "Nuca":
+if personName.lower() != "nuca":
     print("Then your birthday is Not important xD")
+else: 
+  print("You are the Queen of the OCEAN")
